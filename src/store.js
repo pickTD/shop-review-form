9e96ca79862fcbc4516033d1123dfe69ead9f3ce
picktd <vladislav.panferov@gmail.com>
2019-10-08 23:30:55 +0700

    add store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,34 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    reviews: [],
+    reviewIsComposing: {
+      composing: false,
+      shopId: 0,
+    },
   },
   mutations: {
-
+    setData(state, data) {
+      state.reviews = data;
+    },
+    setComposingState(state, value) {
+      state.reviewIsComposing.composing = value;
+    },
+    setComposingShop(state, value) {
+      state.reviewIsComposing.shopId = value;
+    },
+    updateReviews(state, value) {
+      state.reviews = [...state.reviews, value];
+    },
   },
   actions: {
-
+    setComposingStateAction({ commit }, { composing, shopId }) {
+      commit('setComposingState', composing);
+      commit('setComposingShop', shopId);
+    },
+    updateReviewsAction({ commit }, value) {
+      commit('updateReviews', value);
+      commit('setComposingState', false);
+    },
   },
 });
